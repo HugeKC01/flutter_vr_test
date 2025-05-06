66b487c413c1cb24e0a932a3ec0d0966e7b649e2
HugeKC <20868603+HugeKC01@users.noreply.github.com>
2025-05-06 15:59:18 +0700

    pause implement vr mode for image

--- lib/vr_image_viewer.dart
+++ lib/vr_image_viewer.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:panorama_viewer/panorama_viewer.dart';
+import 'package:flutter/services.dart';
 
 class VrImageViewer extends StatefulWidget {
   const VrImageViewer({super.key});
@@ -13,6 +14,10 @@ class VrImageViewer extends StatefulWidget {
 class _VrImageViewerState extends State<VrImageViewer> {
   File? _imageFile;
   final ImagePicker _picker = ImagePicker();
+  bool _isVrMode = false;
+  double _longitude = 0;
+  double _latitude = 0;
+  double _zoom = 1;
 
   Future<void> _pickImage() async {
     final XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery);
@@ -21,6 +26,45 @@ class _VrImageViewerState extends State<VrImageViewer> {
         _imageFile = File(pickedFile.path);
       });
     }
+  }
+
+  void _toggleVrMode() async {
+    if (!_isVrMode) {
+      await SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeLeft,
+        DeviceOrientation.landscapeRight,
+      ]);
+    } else {
+      await SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+        DeviceOrientation.landscapeLeft,
+        DeviceOrientation.landscapeRight,
+      ]);
+    }
+    setState(() {
+      _isVrMode = !_isVrMode;
+    });
+  }
+
+  void _onViewChanged(double longitude, double latitude, double zoom) {
+    setState(() {
+      _longitude = longitude;
+      _latitude = latitude;
+      _zoom = zoom;
+    });
+  }
+
+  @override
+  void dispose() {
+    // Restore orientation when leaving
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    super.dispose();
   }
 
   @override
@@ -34,9 +78,36 @@ class _VrImageViewerState extends State<VrImageViewer> {
                     onPressed: _pickImage,
                     child: const Text('Pick Image'),
                   )
-                : PanoramaViewer(
-                    child: _imageFile != null ? Image.file(_imageFile!) : null,
-                  ),
+                : _isVrMode
+                    ? Row(
+                        children: [
+                          Expanded(
+                            child: PanoramaViewer(
+                              child: Image.file(_imageFile!),
+                              longitude: _longitude,
+                              latitude: _latitude,
+                              zoom: _zoom,
+                              onViewChanged: _onViewChanged,
+                            ),
+                          ),
+                          Expanded(
+                            child: PanoramaViewer(
+                              child: Image.file(_imageFile!),
+                              longitude: _longitude,
+                              latitude: _latitude,
+                              zoom: _zoom,
+                              onViewChanged: _onViewChanged,
+                            ),
+                          ),
+                        ],
+                      )
+                    : PanoramaViewer(
+                        child: Image.file(_imageFile!),
+                        longitude: _longitude,
+                        latitude: _latitude,
+                        zoom: _zoom,
+                        onViewChanged: _onViewChanged,
+                      ),
           ),
           Positioned(
             top: 32,
@@ -59,6 +130,29 @@ class _VrImageViewerState extends State<VrImageViewer> {
               ),
             ),
           ),
+          if (_imageFile != null)
+            Positioned(
+              top: 32,
+              right: 24,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.7),
+                  borderRadius: BorderRadius.circular(24),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.3),
+                      blurRadius: 12,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: IconButton(
+                  icon: Icon(_isVrMode ? Icons.close : Icons.vrpano, color: Colors.white),
+                  onPressed: _toggleVrMode,
+                  tooltip: _isVrMode ? 'Exit VR Mode' : 'Enter VR Mode',
+                ),
+              ),
+            ),
         ],
       ),
       floatingActionButton: _imageFile != null
